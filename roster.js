708e9bd5515ab745ed9144828a9cba0631ff1570
Brodie Lee <brodieslee@gmail.com>
2024-11-11 22:51:24 -0500

    lonny fix

--- roster.js
+++ roster.js
@@ -6,6 +6,53 @@ menu.addEventListener("click", function () {
   menuLinks.classList.toggle("active");
 });
 
+const players = [
+  {
+    lastname: "lee",
+    name: "Brodie Lee",
+    year: 2026,
+    pos: 4,
+    position: "Lock",
+    hometown: "Brookline, MA",
+  },
+  {},
+];
+
+function createCard(player) {
+  return `
+  <div class="card__container animate__animated animate__slideInUp animate__faster" data-lastname="${player.lastname}" data-year="${player.year}" data-pos="${player.pos}">
+    <div class="image-container">
+      <img src="${player.image}" alt="Player Image" />
+    </div>
+    <div class="profile__container">
+      <div class="profile-header">
+        <h1 class="name">${player.name}</h1>
+        <h2 class="board-position"><i>${player.position}</i></h2>
+      </div>
+      <table class="details">
+        <tr>
+          <th class="position">Position:</th>
+          <th class="year">Year:</th>
+          <th class="hometown">Hometown:</th>
+        </tr>
+        <tr>
+          <td class="player_position">${player.position}</td>
+          <td class="player_year">${player.year}</td>
+          <td class="player_hometown">${player.hometown}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+`;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const playerList = document.getElementById("player__list");
+  players.forEach((player) => {
+    playerList.innerHTML += createCard(player);
+  });
+});
+
 function show_list() {
   var courses = document.getElementById("courses_id");
 
